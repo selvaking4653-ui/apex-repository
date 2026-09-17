e99897eda5327a86fc095d41faf1ca04f491cf6b
Sivagami S <selvaking4653@gmail.com>
2026-09-17 09:53:42 +0530

    Add files via upload

---
+++ arithmetic operation.py
@@ -0,0 +1,10 @@
+a = 15
+b = 10
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+print("Modulus =", a % b)
+print("Floor Division =", a // b)
+print("Exponent =", a ** b)
